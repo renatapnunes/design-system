f7fdb617e73096f10029cacad1561fc5b8a8df5f
renas <renatanunes231@yahoo.com.br>
2022-10-13 10:50:36 -0300

    Changed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export function App() {
   return (
     <>
       <h1 className="font-bold text-2xl text-violet-500">Hellow World!</h1>
+      <h2 className="font-bold text-xl text-violet-500">Design System</h2>
       <button className="bg-gradient-to-r from-green-400 via-teal-300 to-sky-400 font-semibold text-sm px-4 py-2 rounded hover:border-solid hover:border-2 hover:border-react-color">
         Enviar
       </button>
